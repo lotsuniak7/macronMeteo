92ce5fc32ee504240ae3b90a280d43c85ec88d84
lotsuniak7 <ivanuska961@gmail.com>
2025-03-27 16:27:54 +0100

    now if you put city in uppercase it go change to first lettre upper and other lower

--- views/weatherView.php
+++ views/weatherView.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <title>MacronWeather</title>
     <link rel="stylesheet" href="../styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>Weather with Macron</h1>
-    <form method="post" action="../controller/indexController.php">
-        <label for="city">Put a city</label>
-        <input type="text" id="city" name="city" placeholder="ex Paris">
+    <form method="post" action="../controller/indexController.php" class="form-city">
+        <label for="city" class="form-label">Put a city</label>
+        <br>
+        <input type="text" id="city" name="city" class="form-control" placeholder="ex Paris">
         <button type="submit">Check the weather</button>
     </form>
     <form method="post" action="../controller/indexController.php">
@@ -29,7 +31,7 @@
                 echo '<div class="weather-content">';
                 echo '<div><img src="../media/macronBase.png" alt="Макрон" class="macron-img"></div>';
                 echo '<div class="weather-info">';
-                echo '<h2>' . htmlspecialchars($city) . '</h2>';
+                echo '<h2>' . htmlspecialchars(strtoupper($city[0]) . substr(strtolower($city), 1)) . '</h2>';
                 echo '<p>Temperature: ' . htmlspecialchars($weatherData['main']['temp']) . '°C</p>';
                 echo '<p>Weather: ' . htmlspecialchars($weatherData['weather'][0]['description']) . '</p>';
                 echo '<p>Humidity: ' . htmlspecialchars($weatherData['main']['humidity']) . '%</p>';
@@ -52,5 +54,6 @@
         }
         ?>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
