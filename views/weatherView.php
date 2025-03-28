a6b869c616fdb34c5193b2030e4d11632d26462c
lotsuniak7 <ivanuska961@gmail.com>
2025-03-28 16:50:33 +0100

    If weather is clear I will show same image......

--- views/weatherView.php
+++ views/weatherView.php
@@ -24,22 +24,37 @@
 
     <div class="weather-sections">
         <?php
+        // Через foreach получаем погоду города
         foreach ($_SESSION['cities'] as $city) {
             $weatherData = getWeatherData($city, $apiKey);
+            // Если удалось, то показываем контейнер
             if ($weatherData && $weatherData['cod'] == 200) {
                 echo '<div class="weather-card">';
                     echo '<div id="macron">';
                         echo '<img src="../media/macronBase.png" alt="Макрон" class="macron-img">';
                     echo '</div>';
                     echo '<div class="weather-content">';
+                        // Name of city
                         echo '<h2>' . htmlspecialchars(strtoupper($city[0]) . substr(strtolower($city), 1)) . '</h2>';
                         echo '<div class="weather-info">';
                             echo '<article class="section-article-temp">';
+                                // Показываем температуру плюс тут округление с помощью round()
                                 echo '<p>Temperature: ' . htmlspecialchars(round($weatherData['main']['temp'])) . '°C</p>';
-                                echo '<img src="">'; // Laissez l'attribut src vide comme dans votre code
                             echo '</article>';
                             echo '<article class="section-article-weather">';
+                                // Выбираем картинку для отображение погоды которая сейчас с помощью сравнения
+                                // Например если у нас clear sky то показываем картинку солнечно и тд
                                 echo '<p>Weather: ' . htmlspecialchars($weatherData['weather'][0]['description']) . '</p>';
+                                if ($weatherData['weather'][0]['description'] === 'clear sky')
+                                    echo '<img src="../media/sunny.png" alt="Sunny" class="weather-img">';
+                                elseif ($weatherData['weather'][0]['description'] === 'broken clouds' || $weatherData['weather'][0]['description'] === 'overcast clouds')
+                                    echo '<img src="../media/cloudy.png" alt="Cloudy" class="weather-img">';
+                                /*
+                                    Для облегчения просто решил сделать проверку если
+                                    строка содержит слово: snow, то будем показывать фотку со снегом
+                                */
+                                elseif (str_contains($weatherData['weather'][0]['description'], 'snow'))
+                                    echo '<img src="../media/snowy.png" alt="Snow" class="weather-img">';
                             echo '</article>';
                             echo '<article class="section-article-humidity">';
                                 echo '<p>Humidity: ' . htmlspecialchars($weatherData['main']['humidity']) . '%</p>';
